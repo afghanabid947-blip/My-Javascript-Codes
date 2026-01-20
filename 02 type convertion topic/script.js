b6420ec47e9a98a7a351921c3f4e1b66b3c1ff10
Abid ullah <afghanabid947@gmail.com>
2026-01-20 16:22:33 +0500

    Add files via upload

---
+++ 02 type convertion topic/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").onclick = function () {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  let sum = n1 + n2;
+
+  document.getElementById("result").textContent = "Sum: " + sum;
+};
